a116a529935931736052a16f495cbdca85d7c9f2
zhangxuan2011 <zx20110412@outlook.com>
2026-06-14 13:53:47 +0800

    Removed smth

--- tests/logical_not.asl
+++
@@ -1,6 +0,0 @@
-// Check for issue 294 (logical-not broken)
-DefinitionBlock("", "DSDT", 1, "RSACPI", "LOGICNOT", 1) {
-    Method (MAIN, 0, NotSerialized) {
-        Return (!1)
-    }
-}
--- tools/aml_test_tools/src/handlers/mod.rs
+++
@@ -1,9 +0,0 @@
-//! A collection of [`handlers`](acpi::Handler) that might be useful when testing AML.
-//!
-//! These are all used by the [`acpi`] integration tests, or by `aml_tester`.
-
-pub mod check_cmd_handler;
-pub mod listed_response_handler;
-pub mod logging_handler;
-pub mod null_handler;
-pub mod std_test_handler;
